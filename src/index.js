a28ca38316efa43a3449fde46d726c261be17db6
Clooney-dev <561754@mail.ru>
2022-06-19 23:22:59 +0300

    feat: Task: Reverse Int was completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+    let str = n.toString();
+
+    if(str[0] === '-'){
+        let reverseStr = str.replace('-', '').split('').reverse().join('');
+        return reverseStr;
+    }else{
+        let reverseStr = str.split('').reverse().join('');      
+        return reverseStr;  
+    }
+    }
